661554a6da5178f1625a6856adfd3ffc72bf90c7
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2024-01-09 11:09:55 +0600

    for loop added

---
+++ js-basic.js
@@ -0,0 +1,20 @@
+console.log("Hellow I am a Teacher");
+
+// let age = 12;
+// if (age>40){
+//     console.log("you can Drive!");
+// }
+// else if(age == 0){
+//     alert('you are a shit!');
+// }else{
+//     console.log("fuck you!");
+// }
+
+// a = 5
+// b = 6
+// c = a > b ? (a - b) : (b - a)
+// console.log(c);
+
+for (let i = 0; i < 100; i++){
+    console.log(i);
+}
